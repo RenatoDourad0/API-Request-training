9ce50cdfdfeaa74e4bafda36fa5259256a2f0f25
Renato Dourad0 <99191410+RenatoDourad0@users.noreply.github.com>
2022-07-08 19:06:42 -0300

    exercicios 2 e 3

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -1,22 +1,32 @@
 const { saveFavoriteMagicCard } = require('../src/magic.js');
 const favoriteCards = require('../data/favoriteCards.js');
 
-const retrievesFavoriteCards = () => {
-  // implemente sua função aqui
+const retrievesFavoriteCards = (elem) => {
+  return elem.map((curr) => curr.name)
 }
 
 describe(' Testa a função saveFavoriteMagicCard', () => {
+  afterEach(() => {
+    favoriteCards.splice(-2, 2)
+    return favoriteCards
+  })
   it('Testa se um novo card é adicionado a cada execução', async () => {
-    expect.assertions();
+    expect.assertions(3);
     await saveFavoriteMagicCard('130553');
-
-    // implemente seus testes aqui
+    expect(favoriteCards.length).toBe(5);
+    expect(favoriteCards[favoriteCards.length - 1].name).toBe('Beacon of Immortality');
+    await saveFavoriteMagicCard('130554');
+    expect(favoriteCards.length).toBe(6);
   });
 
   it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect.assertions();
-    // expect(favoriteCards).toHaveLength(4);
-
-    // implemente seus testes aqui
+    expect.assertions(2);
+    expect(favoriteCards).toHaveLength(4);
+    expect(retrievesFavoriteCards(favoriteCards)).toEqual([
+      "Ancestor's Chosen",
+      'Angel of Mercy',
+      'Aven Cloudchaser',
+      'Ballista Squad'
+    ])
   });
 });
